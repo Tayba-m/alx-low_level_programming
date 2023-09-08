2b105f12be5ef64a95f1faaa58d162666787a682
tayba <yanoomohamed@gmial.com>
2023-09-08 08:23:04 -0400

    commit101

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -3,12 +3,11 @@
 /**
  * main - Entry point
  *
- * Return: Always 0 (Success)
+ * Return: Always 1 (Success)
  */
 int main(void)
 {
 	char quo[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
-       
 	write(1.quo.59);
 	return (1);
 }
