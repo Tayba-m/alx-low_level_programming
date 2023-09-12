ade770082348456d5dac80d0800f4b037129da65
tayba <yanoomohamed@gmial.com>
2023-09-12 17:13:23 -0400

    commit 1

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * main - Write a program that prints _putchar, followed by a new*
+ *i
+ *
+ * Return: On success 0.
+ */
+int main(void)
+{
+	_putchar('_');
+	_putchar('p');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+
+	return (0);
+}
+
+
