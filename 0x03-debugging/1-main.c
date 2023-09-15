fc4f67a1d04931a9e553c3e57e92e54c71460110
tayba <yanoomohamed@gmial.com>
2023-09-15 15:35:32 -0400

    commit1

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -4,14 +4,13 @@
 * main - causes an infinite loop
 * Return: 0
 */
-
 int main(void)
 {
-        int i;
+	int i;
 
-        printf("Infinite loop incoming :(\n");
+	printf("Infinite loop incoming :(\n");
 
-        printf("Infinite loop avoided! \\o/\n");
+	printf("Infinite loop avoided! \\o/\n");
 
-        return (0);
+	return (0);
 }
