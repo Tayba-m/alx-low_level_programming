6135a3906fbfb49df924089b4096f016dec86af2
tayba <yanoomohamed@gmial.com>
2023-09-18 06:58:56 -0400

    commit 2

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -1,17 +1,15 @@
-#include <stdio.h>
-#include <main.h>
+#include "main.h"
 /**
- * void swap_int - set the integer to 402
- * @a @b: a pointer that swaps the values
- *
+ * swap_int - swaps the values of two integers
+ * @a: a pointer that swaps the values
+ * @b :a pointer that swap the values
  * Return: nothing
  */
 void swap_int(int *a, int *b)
 {
-   int c;
+	int c;
 
 	c = *a;
 	*a = *b;
 	*b = c;
-}
 }
