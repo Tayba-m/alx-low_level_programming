d7cebe7b9f0c0926150394db485d2febb1af1e59
tayba <yanoomohamed@gmial.com>
2023-09-27 05:19:03 -0400

    commit1

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * _strcat - concatenates two strings
+ * @dest: input value
+ * @src: input value
+ *
+ * Return: dest
+ */
+char *_strcat(char *dest, char *src)
+{
+	int s1 = 0;
+	int s2 = 0;
+
+	while (dest[s1] != '\0')
+	{
+		s1++;
+	}
+	while (src[s2] != '\0')
+	{
+		dest[s1] = src[s2];
+		s1++;
+		s2++;
+	}
+	dest[s1] = '\0';
+	return (dest);
+}
