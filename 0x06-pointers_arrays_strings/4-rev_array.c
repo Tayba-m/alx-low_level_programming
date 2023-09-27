6b6c70ee0558761784aa6b6b0ec6f87892c5d92e
tayba <yanoomohamed@gmial.com>
2023-09-27 06:35:33 -0400

    commit6

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * reverse_array - reverse array of integers
+ * @a: array
+ * @n: number of elements of array
+ * Return: void
+ */
+void reverse_array(int *a, int n)
+
+{
+	int x, z;
+
+	for (x = 0; x < n--; x++)
+	{
+		z = a[x];
+		a[x] = a[n];
+		a[n] = z;
+	}
+}
