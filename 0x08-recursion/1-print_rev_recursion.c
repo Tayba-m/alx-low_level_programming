8e362a1961a4e92379f78da9d7d5bc4ceedb6703
tayba <yanoomohamed@gmial.com>
2023-09-28 11:51:03 -0400

    commit3

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -1,17 +1,17 @@
 #include "main.h"
 /**
  * _print_rev_recursion - prints a string in reverse
- * @s : 
+ * @s : arg
  *
  */
 
 void _print_rev_recursion(char *s)
 {
-	
-	if (*s == \0)
+
+	if (*s)
 	{
-		_putchar(s);
-		_print_rev_recursion(*s - 1);
+		_print_rev_recursion(s + 1);
+		_putchar(*s);
 
 	}
 }
