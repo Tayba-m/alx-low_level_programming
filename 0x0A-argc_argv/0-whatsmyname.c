0b4fe579f9725c31ce242196c0ecffee2a454a72
tayba <yanoomohamed@gmial.com>
2023-10-02 21:08:12 -0400

    commit1

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - program that prints its name, followed by a new line
+ * @argc: number of arguments
+ * @argv: array with the arguments
+ *
+ * Return: always 0
+ **/
+
+int main(int argc_, char const *argv[])
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
