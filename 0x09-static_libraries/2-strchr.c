1753e3ffb1fc0f98197aa9ca53778fa862b2d7fa
tayba <yanoomohamed@gmial.com>
2023-10-02 20:30:02 -0400

    commit1

---
+++ 0x09-static_libraries/2-strchr.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _strchr - Entry point
+ * @s: input
+ * @c: input
+ * Return: Always 0 (Success)
+ */
+char *_strchr(char *s, char c)
+{
+	for (int a = 0; s[a] >= '\0'; a++)
+	{
+		if (s[a] == c)
+		return (&s[a]);
+	}
+	return (0);
+}
