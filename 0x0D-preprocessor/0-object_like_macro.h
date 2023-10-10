d593705f3b8a2394706990038077bb7433e465cc
tayba <yanoomohamed@gmial.com>
2023-10-10 10:57:32 -0400

    commit

--- 0x0D-preprocessor/0-object_like_macro.h
+++ 0x0D-preprocessor/0-object_like_macro.h
@@ -1,4 +1,6 @@
 #ifndef _OPJECT_LIKE_MACRO_H
 #define _OPJECT_LIKE_MACRO_H
+
 #define SIZE 1024
-#endif
+
+#endif/**opject**/
--- 0x0D-preprocessor/1-pi.h
+++ 0x0D-preprocessor/1-pi.h
@@ -1,4 +1,6 @@
 #ifndef _PI_H
 #define _PI_H
+
 #define IP 3.14159265359
-#endif
+
+#endif/**_pi**/
