653f94698d3c1ee7c32bc12dd28c2534cd6bd241
tayba <yanoomohamed@gmial.com>
2023-10-11 03:29:48 -0400

    commit

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -11,8 +11,8 @@ void print_dog(struct dog *d)
 {
 	if (d)
 	{
-		printf("Name: %s\n", d->name ? name : "(nil)");
+		printf("Name: %s\n", d->name ? d->name : "(nil)");
 		printf("Age: %f\n", age);
-		printf("Owner: %s\n", d->owner ? owner : "(nil)");
+		printf("Owner: %s\n", d->owner ? d->owner : "(nil)");
 	}
 }
