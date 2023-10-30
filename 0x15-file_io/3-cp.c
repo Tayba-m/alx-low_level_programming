ce040d38a6296743320a59734b9278843ed10fa9
Tayba-m <yanoomohamed@gmial.com>
2023-10-30 10:51:33 -0400

    commit

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -6,10 +6,10 @@ char *create_buffer(char *file);
 void close_file(int fd);
 
 /**
- * creates_buffer - function.
- * @file: name of the file buffer
+ * creates_buffer - Allocates 1024 bytes for a buffer.
+ * @file: The name of the file buffer is storing chars for.
  *
- * Return: pointer to buffer.
+ * Return: A pointer to the newly-allocated buffer.
  */
 char *creates_buffer(char *file)
 {
@@ -28,28 +28,33 @@ char *creates_buffer(char *file)
 }
 
 /**
- * close_file - Closes file
- * @v: file to be closed.
+ * close_file - Closes file descriptors.
+ * @fd: The file descriptor to be closed.
  */
-void close_file(int v)
+void close_file(int fd)
 {
 	int c;
 
-	c = close(v);
+	c = close(fd);
 
 	if (c == -1)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", v);
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd);
 		exit(100);
 	}
 }
 
 /**
- * main - Copies the contents of a file
- * @argc: number of arguments
- * @argv: pointers to the arguments.
+ * main - Copies the contents of a file to another file.
+ * @argc: The number of arguments supplied to the program.
+ * @argv: An array of pointers to the arguments.
  *
  * Return: 0 on success.
+ *
+ * Description: If the argument count is incorrect - exit code 97.
+ * If file_from does not exist or cannot be read - exit code 98.
+ * If file_to cannot be created or written to - exit code 99.
+ * If file_to or file_from cannot be closed - exit code 100.
  */
 int main(int argc, char *argv[])
 {
@@ -96,4 +101,3 @@ int main(int argc, char *argv[])
 
 	return (0);
 }
-
